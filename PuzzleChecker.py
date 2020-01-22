c0fa7aaea06e0354f6cb713dec92faec06619182
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-01-22 20:16:19 +0100

    Working puzzle checker

--- PuzzleChecker.py
+++ PuzzleChecker.py
@@ -1,8 +1,15 @@
 import numpy as np
+import itertools
+
+
+def transpose_and_yield_top(arr):
+    while len(arr) > 0:
+        yield arr[0]
+        arr = list(reversed(list(zip(*arr[1:]))))
 
 
 def is_puzzle_solvable(puzzle):
-    flat_puzzle = puzzle.puzzle.flatten()
+    flat_puzzle = list(itertools.chain(*transpose_and_yield_top(puzzle.puzzle)))
     empty_frame_row = 0
     inversions = 0
 
@@ -21,7 +28,7 @@ def is_puzzle_solvable(puzzle):
             if first_frame > second_frame:
                 inversions += 1
     
-    if puzzle.dimension % 2 == 0:
+    if puzzle.dimension % 2 != 0:
         return inversions % 2 == 0
     else:
         if puzzle.dimension - empty_frame_row % 2 == 0:
