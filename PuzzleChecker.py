d255c5d91a0439bfe1b8e9760dd5252cf7f68b3e
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-01-23 12:08:33 +0100

    Working puzzle checker

--- PuzzleChecker.py
+++ PuzzleChecker.py
@@ -2,14 +2,22 @@ import numpy as np
 import itertools
 
 
-def transpose_and_yield_top(arr):
-    while len(arr) > 0:
-        yield arr[0]
-        arr = list(reversed(list(zip(*arr[1:]))))
+def unidimensional_array(puzzle_array):
+    if puzzle_array.shape[1] == 1:
+        return puzzle_array
+    
+    rotated_puzzle = puzzle_array
+    rotated_puzzle = np.delete(rotated_puzzle, 0, 0)
+    rotated_puzzle = np.rot90(rotated_puzzle)
+
+    top_row = puzzle_array[0]
+    next_row = unidimensional_array(rotated_puzzle)
+
+    return np.append(top_row, next_row)
 
 
 def is_puzzle_solvable(puzzle):
-    flat_puzzle = list(itertools.chain(*transpose_and_yield_top(puzzle.puzzle)))
+    flat_puzzle = unidimensional_array(puzzle.puzzle)
     empty_frame_row = 0
     inversions = 0
 
