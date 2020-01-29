bfe0d8584cf3a9a81b8299ca3306783cb749ddaf
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-01-29 17:51:30 +0100

    Working

--- PuzzleParser.py
+++ PuzzleParser.py
@@ -2,17 +2,29 @@ import PyKit
 import numpy as np
 from Puzzle import Puzzle
 
+def puzzle_gen(size):
+    puzzle_array = np.arange(size * size)
+    np.random.shuffle(puzzle_array)
+    puzzle_array = puzzle_array.reshape(size, size)
+    return puzzle_array
+
+
 def get_puzzle_from_file_name(file_name):
     file_content = PyKit.FileManager.get_string_from_file(file_name)
     parser = PyKit.Parser(clear_empty_lines=True, clear_comment_lines=True)
     parsed_lines = parser.parsed_str(file_content)
-    n = int(parsed_lines[0])
-    puzzle = np.zeros((n, n))
 
-    del parsed_lines[0]
+    if len(parsed_lines) == 1 and len(parsed_lines[0]) == 1:
+        dimension = int(parsed_lines[0])
+        return Puzzle(puzzle_gen(dimension), dimension)
+    else:
+        n = int(parsed_lines[0])
+        puzzle = np.zeros((n, n))
 
-    for (index, line) in enumerate(parsed_lines):
-        np_line = np.fromstring(line, dtype=int, sep=" ")
-        puzzle[index] = np_line
+        del parsed_lines[0]
 
-    return Puzzle(puzzle, n)
+        for (index, line) in enumerate(parsed_lines):
+            np_line = np.fromstring(line, dtype=int, sep=" ")
+            puzzle[index] = np_line
+
+        return Puzzle(puzzle, n)
--- n-puzzle.py
+++ n-puzzle.py
@@ -6,6 +6,8 @@ import Uniform_cost_search
 import Greedy_search
 import Display_solution
 import Heuristic
+import numpy as np
+from Puzzle import Puzzle
 from End_Puzzle import puzzle_solution_generator
 
 
@@ -20,14 +22,35 @@ def heuristic_function(flag_value):
         return None
 
 
+def resolve_algorithm(flag_value, puzzle, puzzle_solution, function, indexes_solution):
+    if flag_value == "a*":
+        return A_star.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution, function, indexes_solution)
+    elif flag_value == "uniform_cost":
+        return Uniform_cost_search.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution)
+    elif flag_value == "greedy_search":
+        return Greedy_search.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution, function, indexes_solution)
+    else:
+        return None
+
+
 if __name__ == "__main__":
     PyKit.CommandLine.register_usage("usage: python n-puzzle [file_name]")
-    PyKit.CommandLine.register_flag("h", "The heurisitic function that will be used to solve the puzzle. Can use hamming, manhattan or linear. Default is hamming.", default_value="hamming")
+    PyKit.CommandLine.register_flag("h", "The heurisitic function that will be used to solve the puzzle. Can use hamming, manhattan or linear. Default is manhattan.", default_value="manhattan")
+    PyKit.CommandLine.register_flag("a", "The algorithm that will be used to solve the puzzle. Can use a*, uniform_cost or greedy_search. Default is a*.", default_value="a*")
     PyKit.CommandLine.show_usage_if_needed()
 
     file_name = PyKit.CommandLine.get_argument_at_index(1)
     heuristic = PyKit.CommandLine.get_value_for_flag("h")
-    puzzle = PuzzleParser.get_puzzle_from_file_name(file_name)
+
+    if file_name.isdigit() and int(file_name) > 2:
+        dimension = int(file_name)
+        puzzle_array = PuzzleParser.puzzle_gen(dimension)
+        puzzle = Puzzle(puzzle_array, dimension)
+    else:
+        try:
+            puzzle = PuzzleParser.get_puzzle_from_file_name(file_name)
+        except:
+            PyKit.Display.error("This puzzle is not correctly formatted.")
     
     function = heuristic_function(heuristic)
     if function is None:
@@ -37,8 +60,16 @@ if __name__ == "__main__":
         PyKit.Display.error("This puzzle is not solvable.")
 
     puzzle_solution, indexes_solution = puzzle_solution_generator(puzzle.dimension)
-    # result = A_star.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution, function, indexes_solution)
-    # result = Uniform_cost_search.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution)
-    result = Greedy_search.resolve(puzzle.puzzle, puzzle.dimension, puzzle_solution, function, indexes_solution)
+
+    algo = PyKit.CommandLine.get_value_for_flag("a")
+
+    try:
+        result = resolve_algorithm(algo, puzzle, puzzle_solution, function, indexes_solution)
+    except:
+        PyKit.Display.error("This puzzle is not correctly formatted.")
+
+    if (result is None):
+        PyKit.Display.error(algo + " is not a valid algorithm. Use a*, uniform_cost or greedy_search")
+
     result[0].puzzle = puzzle.puzzle
     Display_solution.start(*result)
